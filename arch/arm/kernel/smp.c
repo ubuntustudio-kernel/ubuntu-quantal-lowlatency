7512672dd8663b9b9b38ce04fe31c65cca9f481b
Santosh Shilimkar <santosh.shilimkar@ti.com>
2010-07-13 17:36:23 +0530

    ARM: smp: Skip non-boot CPU's calibration in hotplug path
    The current calibrate the lpj every time CPU hotplug path. This
    delays CPU online process.
    
    We can use master CPU lpj and assign it to the onlined non-boot CPUs
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Sricharan R <r.sricharan@ti.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -225,6 +225,7 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 {
 	struct mm_struct *mm = &init_mm;
 	unsigned int cpu;
+	static bool booted;
 
 	/*
 	 * The identity mapping is uncached (strongly ordered), so
@@ -256,7 +257,9 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 
 	notify_cpu_starting(cpu);
 
-	calibrate_delay();
+	if (!booted)
+		calibrate_delay();
+	booted = true;
 
 	smp_store_cpu_info(cpu);
 
